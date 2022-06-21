4fd3c665ea76f401aca63b6b7775f018a4bead98
WaifuYuezhengLing <lyushangyushukatu@gmail.com>
2022-06-21 17:14:20 +0900

    プレイヤー移動完了、敵キャラ準備

---
+++ UnityPro/OpenNinja/Assets/PlayrtControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayrtControl : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0.05f, 0, 0);
+        Movement();
+    }
+    void Movement()
+    {
+        float speedDown = Input.GetAxis("Horizontal");
+        transform.Translate(0.025f * speedDown, 0, 0);
+    }
+}
