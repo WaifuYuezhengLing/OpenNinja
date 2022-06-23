352dccdcaf3abbaedfc53c71351cde7112a12704
WaifuYuezhengLing <lyushangyushukatu@gmail.com>
2022-06-23 17:47:14 +0900

    隠れる機能、カメラ追従

---
+++ UnityPro/OpenNinja/Assets/Forcamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forcamera : MonoBehaviour
+{
+    float offset;
+    
+    [SerializeField] GameObject PlayerPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position.x - PlayerPos.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(PlayerPos.transform.position.x + offset, transform.position.y,transform.position.z);
+    }
+}
--- UnityPro/OpenNinja/Assets/PlayrtControl.cs
+++ UnityPro/OpenNinja/Assets/PlayrtControl.cs
@@ -5,21 +5,46 @@ using UnityEngine;
 public class PlayrtControl : MonoBehaviour
 {
     Rigidbody2D rb;
+    public bool hiding;
+    
     // Start is called before the first frame update
     void Start()
     {
+        hiding = false;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.05f, 0, 0);
-        Movement();
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            hiding = false;
+            transform.position = new Vector3(0, -2, 0);
+        }
+        if (hiding == false)
+        {
+            transform.Translate(0.05f, 0, 0);
+            Movement();
+        }
+        
     }
     void Movement()
     {
         float speedDown = Input.GetAxis("Horizontal");
         transform.Translate(0.025f * speedDown, 0, 0);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Window")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                hiding = true;
+                transform.position = collision.gameObject.transform.position;
+            }
+            
+        }
+        
     }
 }
